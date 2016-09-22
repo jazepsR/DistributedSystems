1d8ae679d2d423dd1faf9e7a0a207bb7fdc3b59b
Jazeps Rutkis <jazepsrutkis93@gmail.com>
2016-09-22 19:20:00 +0200

    Revert "fixed server port numbers"
    This reverts commit 7ddc1b3a3643999711e5051db70253bb75161389.

--- DSProject/src/main/java/com/company/Main.java
+++ DSProject/src/main/java/com/company/Main.java
@@ -7,8 +7,8 @@ public class Main {
         //Server server = new Server(14);
         //Client client1 = new Client("servert",14,"Jekabs");
         //Client client2 = new Client("server",14,"Antons");
-        ServerGUI sgui = new ServerGUI(143);
-        ClientGUI cgui1 = new ClientGUI("localhost",143);
+        ServerGUI sgui = new ServerGUI(141);
+        ClientGUI cgui1 = new ClientGUI("localhost",142);
         ClientGUI cgui2 = new ClientGUI("localhost",143);
     }
 }
