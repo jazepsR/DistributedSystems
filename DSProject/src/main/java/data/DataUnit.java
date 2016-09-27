fde2c6e9f58ad9815491d9336fd4927697df5057
Euaggelos Karountzos <Euaggelos Karountzos>
2016-09-27 12:20:35 +0300

    data objects

---
+++ DSProject/src/main/java/data/DataUnit.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package data;
+
+/**
+ *
+ * @author Angelo
+ */
+public class DataUnit {
+    
+    private final String ipAddress;
+    private final String macAddress;
+    private final MessageType msgType;
+    
+    public DataUnit(String ipAddress, String macAddress, MessageType msgType){
+        this.ipAddress = ipAddress;
+        this.macAddress = macAddress;
+        this.msgType = msgType;
+    }
+    
+    public String getIpAddress(){
+        return this.ipAddress;
+    }
+    
+    public String getMacAddress(){
+        return this.macAddress;
+    }
+    
+    public MessageType getMsgType(){
+        return this.msgType;
+    }
+    
+}
