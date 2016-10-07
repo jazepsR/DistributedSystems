f1925390f6bcf9e22cd1426485a1ba7b8522d95e
Jazeps Rutkis <jazepsrutkis93@gmail.com>
2016-10-07 17:58:59 +0200

    small fix

--- DSProject/src/main/java/main/Node.java
+++ DSProject/src/main/java/main/Node.java
@@ -64,9 +64,9 @@ public class Node extends Thread {
             }
             
             // test become a leader
-            WaitTimer timer2= new WaitTimer(15);
+            WaitTimer timer2= new WaitTimer(15,bullyAlgo);
             timer2.run();
-            BullyAlgo.bullyThem();
+            bullyAlgo.bullyThem();
         }
         else {
             if(send==2)
