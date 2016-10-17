1695baa697fde9584af07bf003bc0b2fad8c0dad
Angelo <e.karountzos@hotmail.com>
2016-10-17 09:56:51 +0300

    Ordering working

--- DSProject/src/main/java/main/Tree.java
+++ DSProject/src/main/java/main/Tree.java
@@ -19,7 +19,7 @@ public class Tree implements Serializable, Comparable<Tree>{
     
     private InetAddress ipLeader;
     private HashMap<InetAddress, Integer> hmap = new HashMap<InetAddress, Integer>();
-    private HashMap<InetAddress, Integer> Reliability = new HashMap<InetAddress, Integer>();
+     private HashMap<InetAddress, Integer> reliability = new HashMap<InetAddress, Integer>();
 
     /**
      * @return the IPLeader
@@ -34,15 +34,25 @@ public class Tree implements Serializable, Comparable<Tree>{
     public void setIPLeader(InetAddress ip) {
         ipLeader = ip;
     }
+    
+    public void setIPLeader(String ip){
+        setIPLeader(Parser.strToInet(ip));
+    }
+    
+    public void increaseCounter(InetAddress ip){
+        changeCounter(ip, getCounter(ip) + 1);
+    }
+    
+    public void increaseCounter(String ip){
+        changeCounter(ip, getCounter(ip) + 1);
+    }
 
     /**
-     * @return the hmap
+     * 
+     * @return reliability
      */
-    public HashMap<InetAddress, Integer> getReliability() {
-        return Reliability;
-    }
     public HashMap<InetAddress, Integer> getHmap(){
-        return Reliability;
+        return reliability;
     }
     
     /**
@@ -51,7 +61,7 @@ public class Tree implements Serializable, Comparable<Tree>{
      */
     public void addHost(InetAddress ip, int counter) {
         hmap.put(ip,counter);
-        Reliability.put(ip,0);
+        reliability.put(ip,0);
     }
     
     public void addHost(String ip, int counter){
@@ -73,6 +83,10 @@ public class Tree implements Serializable, Comparable<Tree>{
         hmap.put(ip, newCounter);
     }
     
+    public void changeCounter(String ip, int newCounter){
+        changeCounter(Parser.strToInet(ip), newCounter);
+    }
+    
     /**
      * @param ip to get counter
      * @return Counter
@@ -80,9 +94,13 @@ public class Tree implements Serializable, Comparable<Tree>{
     public int getCounter(InetAddress ip) {
         return hmap.get(ip);
     }
+    
+    public int getCounter(String ip){
+        return getCounter(Parser.strToInet(ip));
+    }
 
-    public List<Integer> getVectorClock(){
-        List<Integer> VClock = new ArrayList<Integer>();
+    public ArrayList<Integer> getVectorClock(){
+        ArrayList<Integer> VClock = new ArrayList<Integer>();
         for (Integer value : hmap.values()) {
             VClock.add(value);
         }
@@ -106,12 +124,36 @@ public class Tree implements Serializable, Comparable<Tree>{
     }
 
     public int compareTo(Tree tree) {
-        return  0;
-         //return getVectorClock().compareTo(tree.getVectorClock());
+        ArrayList<Integer> v1 = this.getVectorClock();
+        ArrayList<Integer> v2 = tree.getVectorClock();
+        boolean smallerFound = false, biggerFound = false;
+        
+        for(int i = 0; i < v1.size(); i++){
+            if(v1.get(i) > v2.get(i))
+                smallerFound = true;
+            if(v1.get(i) < v2.get(i))
+                biggerFound = true;
+        }
+        
+        if(smallerFound == true & biggerFound == true)
+            return 0;
+        else if(smallerFound == true & biggerFound == false)
+            return 1;
+        else if(smallerFound == false & biggerFound == true)
+            return -1;
+        return 0;
     }
     
     @Override
     public String toString(){
         return getVectorClock().toString();
+    }
+    
+    public static class Comparators{
+        public static Comparator<Tree> cmp = new Comparator<Tree>(){
+            public int compare(Tree o1, Tree o2) {
+                return o1.compareTo(o2);
+            }
+        };
     }
 }
